61f85146ec8d40da68301317530b9fe19ab23d20
Aldo <aldoyedro181@gmail.com>
2022-11-01 23:39:58 -0300

    Basic UI structure

--- pk-app/pages/index.tsx
+++ pk-app/pages/index.tsx
@@ -1,8 +1,14 @@
 
 export default function Home() {
   return (
-    <div className="">
-      Hello world
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Wich Pokémon is Rounder?</div>
+      <div className="p-2"/>
+      <div className="border rounded p-8 flex justify-between items-center">
+        <div className="w-16 h-16 bg-red-200 max-w-2xl"></div>
+        <div className="p-8">VS</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+      </div>
     </div>
   )
 }
